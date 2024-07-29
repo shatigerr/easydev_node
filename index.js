4bfe81f54b35b265daa4396f0457e1f38683170c
Shatigerr <pauamat888@gmail.com>
2024-07-29 17:46:38 +0200

    Added Logs feature

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const host = "https://easydevapi.onrender.com/"
+//const host = "https://easydevapi.onrender.com/"
+const host = "http://localhost:5102/"
 
 app.get("/:key/:projectid/:endpointURL",  (req,res) => {
     let params = req.query
@@ -30,12 +31,24 @@ app.put("/:key/:projectid/:endpointURL", (req,res) => {
 
 async function executeApi(object,req,res)
 {
+    let status = "200";
+    let logObject = {
+        type:req.method,
+        status:status,
+        IdUser: null,
+        IdProject:req.params["projectid"],
+        query:"",
+        requestDuration:1
+    }
     try {
+        
         let key = req.params["key"];
         let projectid = req.params["projectid"];
         let endpointid = req.params["endpointURL"];
 
         
+        
+
         let par = "";
         for (const key in object) {
             
@@ -50,17 +63,27 @@ async function executeApi(object,req,res)
         let dataResponse = await fetch(`${host}api/Project/details/${projectid}`);
         
         let result = await dataResponse.json();
+        console.log(result["idUser"]);
+        logObject.IdUser = result["idUser"]
+        
+        
 
         // Check key
         if (key !== result.key) {
-            return res.status(401).send("INCORRECT KEY");
+            logObject.status = "401";
+            res.status(401).send("INCORRECT KEY");
+            await logRequest(logObject)    
         }
 
         let endpoint = result.endpoints.filter(e => e.url == endpointid);
-
+        
         if (endpoint.length === 0) {
-            return res.status(404).send("ENDPOINT NOT FOUND");
+            status = "404"
+            res.status(404).send("ENDPOINT NOT FOUND");
+            await logRequest(logObject)
         }
+
+        logObject.query = endpoint[0].query;
 
         
         // Prepare request object
@@ -80,26 +103,52 @@ async function executeApi(object,req,res)
         });
 
         if (!requestResponse.ok) {
-            throw new Error(`Error in request response: ${requestResponse.statusText}`);
+            status = "500"
+            res.send("ERROR PROCESSING THE REQUEST")
+            await logRequest(logObject)
         }
 
         let requestResponseData = await requestResponse.json();
 
         // Log and send response
+
+        
+
         console.log(requestResponseData);
+        
         if(req.method == "GET")
         {
             res.send(requestResponseData)
+            await logRequest(logObject)
+            
         }else{
             res.send({"rowsAffected":requestResponseData});
+            await logRequest(logObject)
         }
         
 
     } catch (error) {
+        status = "500"
+        await logRequest(logObject)
         console.error("Error in request processing:", error);
         res.status(500).send(`Error in request processing: ${error.message}`);
     }
 }
+
+async function logRequest(logObject) {
+    try {
+        await fetch(`${host}api/Log`, {
+            method: 'POST',
+            body: JSON.stringify(logObject),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    } catch (logError) {
+        console.error("Error logging request:", logError);
+    }
+}
+
 
 app.listen("8080",() => {
     console.log("PORT 8080");
